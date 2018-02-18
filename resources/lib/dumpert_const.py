4c1e4fed5bf708eb2f1af08410716a4694f95098
skipmodea1 <skipmodea1@outlook.com>
2018-02-18 09:50:15 +0100

    v1.1.6-SNAPSHOT (2018-02-18)
    - added 'videos' to icon

--- resources/lib/dumpert_const.py
+++ resources/lib/dumpert_const.py
@@ -14,8 +14,8 @@ ADDON = "plugin.video.dumpert"
 SETTINGS = xbmcaddon.Addon(id=ADDON)
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
-DATE = "2018-01-20"
-VERSION = "1.1.5"
+DATE = "2018-02-18"
+VERSION = "1.1.6-SNAPHOST"
 
 
 if sys.version_info[0] > 2:
